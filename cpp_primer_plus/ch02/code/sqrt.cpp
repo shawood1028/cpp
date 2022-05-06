840992e884f8155115f7e6c33be2cb21ad094321
shawood <1127295751@qq.com>
2022-05-06 12:03:32 +0800

    add c++primerplus ch02

---
+++ cpp_primer_plus/ch02/code/sqrt.cpp
@@ -0,0 +1,14 @@
+// sqrt.cpp -- using sqrt() function
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+	using namespace std;
+	double area;
+	cin >> area;
+	double side;
+	side = sqrt(area);
+	cout << side;
+	return 0;
+}
